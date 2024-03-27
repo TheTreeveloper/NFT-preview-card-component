b45272f6882e5acd72e9522481c5e8cccc50c751
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:45:00 +0100

    Created hero-image.jsx

---
+++ react-app/src/hero-image.jsx
@@ -0,0 +1,7 @@
+export const MyImage = () => {
+    return (
+        <div className="hero-image">
+            <img src="./src/assets/images/image-equilibrium.jpg" alt="equilibrium image" />
+        </div>
+    );
+}

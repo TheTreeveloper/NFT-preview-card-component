0986a4329df88c331641c87c5bae5fa059b0a654
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:09:37 +0100

    imported the Creator component and exported App

---
+++ react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Creator} from "./creator";
+
+function App () {
+    return (
+        <>
+        <h1>Hello World</h1>
+        <Creator name = "Jules Wyvern"/>
+        </>
+    );
+}
+
+export default App;

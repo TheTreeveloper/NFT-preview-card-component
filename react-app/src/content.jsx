ff874f8016e4c8e116c19bbfdb8ce93ee4de4b5a
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:52:21 +0100

    Created Content component

---
+++ react-app/src/content.jsx
@@ -0,0 +1,21 @@
+export const Content = () => {
+    return (
+        <>
+        <div className="content-container">
+            <h1>Equilibrium #3429</h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+
+            <div className="eth-info">
+                <div className="eth-info">
+                    <img src="./src/assets/images/icon-ethereum.svg" alt="Ethereum icon image" />
+                    <p className="eth">0.041 ETH</p>
+                </div>
+                <div className="days-left">
+                    <img src="./src/assets/images/icon-clock.svg" alt="Clock image" />
+                    <p className="duration">3 days left</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}

0aaaab5bceaf80a820364de78e6663c974418bd9
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:56:23 +0100

    changed div.eth-info to div.content-info

--- react-app/src/content.jsx
+++ react-app/src/content.jsx
@@ -5,7 +5,7 @@ export const Content = () => {
             <h1>Equilibrium #3429</h1>
             <p>Our Equilibrium collection promotes balance and calm.</p>
 
-            <div className="eth-info">
+            <div className="content-info">
                 <div className="eth-info">
                     <img src="./src/assets/images/icon-ethereum.svg" alt="Ethereum icon image" />
                     <p className="eth">0.041 ETH</p>

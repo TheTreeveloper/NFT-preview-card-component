0d5e0c4d0bd0aab44effca1706f8d6c11c432d21
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:52:37 +0100

    Imported Content component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Creator} from "./creator";
 import { MyImage } from "./hero-image";
+import {Content} from "./content";
 
 function App () {
     return (
         <>
         <div className="parent-container">
             <MyImage />
+            <Content />
             <Creator name = "Jules Wyvern"/>
         </div>
         

ae7be90d5983105726a76a85c694710206208ff1
ioddgamers <toluloperbrts@gmail.com>
2024-03-27 17:43:47 +0100

    Updated App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Creator} from "./creator";
+import { MyImage } from "./hero-image";
 
 function App () {
     return (
         <>
         <div className="parent-container">
+            <MyImage />
             <Creator name = "Jules Wyvern"/>
         </div>
         

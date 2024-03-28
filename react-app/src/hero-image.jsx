92c15c503b2b908ecb8859b214a93db61163b910
ioddgamers <toluloperbrts@gmail.com>
2024-03-28 09:47:20 +0100

    Updated Hero image

--- react-app/src/hero-image.jsx
+++ react-app/src/hero-image.jsx
@@ -2,6 +2,9 @@ export const MyImage = () => {
     return (
         <div className="hero-image">
             <img src="./src/assets/images/image-equilibrium.jpg" alt="equilibrium image" />
+            <div className="overlay">
+                <div className="eye-icon"></div>
+            </div>
         </div>
     );
 }

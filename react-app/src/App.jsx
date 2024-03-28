8ff1f47e3cb9194f7ee6c5aba341d82aa90d33ab
ioddgamers <toluloperbrts@gmail.com>
2024-03-28 09:46:25 +0100

    imported attribution

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import {Creator} from "./creator";
 import { MyImage } from "./hero-image";
 import {Content} from "./content";
+import { Attribution } from "./attribution";
 
 function App () {
     return (
         <>
-        <div className="parent-container">
-            <MyImage />
-            <Content />
-            <Creator name = "Jules Wyvern"/>
+        <div className="main">
+            <div className="parent-container">
+                <MyImage />
+                <Content />
+                <Creator name = "Jules Wyvern"/>
+            </div>
+            <Attribution />
         </div>
         
         </>

8ade52ba1c79cd7860a652cc1b4ca57626e4ef4e
ioddgamers <toluloperbrts@gmail.com>
2024-03-28 09:46:04 +0100

    Added Attribution

---
+++ react-app/src/attribution.jsx
@@ -0,0 +1,8 @@
+export const Attribution = () => {
+    return (
+        <div className="attribution">
+            <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/ioddgamers">Adegbamigbe Tolulope</a>.</p>
+        </div>
+    );
+}
